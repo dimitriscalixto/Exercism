9605f53e799fcca221f79779da5626dd33034c8a
idimitrisc47 <dimitrisc47@gmail.com>
2024-01-04 18:29:51 -0500

    add Acronym Challenge

--- src/Acronym.java
+++ src/Acronym.java
@@ -1,11 +1,26 @@
-class Acronym {
+import java.util.Arrays;
+import java.util.Locale;
 
+class Acronym {
+    private final String phrase;
+    public static void main(String[] args) {
+     Acronym acron = new Acronym("Complementary metal-oxide semiconductor");
+     System.out.println(acron.get());
+    }
     Acronym(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.phrase = phrase;
     }
 
     String get() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String[] arraySplited = phrase.trim().replace("-", " ").replace("_"," ").split(" ");
+        String acronym = "";
+        for(int i = 0;i< arraySplited.length;i++){
+            if(!arraySplited[i].isEmpty()){
+            String ac = Character.toString(arraySplited[i].charAt(0));
+            acronym += ac;
+            }
+        }
+        return acronym.toUpperCase();
     }
 
 }
